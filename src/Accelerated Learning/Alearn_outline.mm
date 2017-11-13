576281133709a8823b076538c7f69844f50f61a0
Ankit <mail@ankit>
2017-11-13 20:47:44 +0530

    Add folder on Accelerated Learning

---
+++ src/Accelerated Learning/Alearn_outline.mm
@@ -0,0 +1,27 @@
+<map version="1.0.0"><node ID="4F80C8C9-9ACB-4667-BD37-4BEC53776708" BACKGROUND_COLOR="#FFFFFF" TEXT="Skill Name" COLOR="#1A5469" POSITION="right" STYLE="bubble"><edge COLOR="#FFFFFF" WIDTH="2" /><font NAME="HelveticaNeue-Light" SIZE="24" /><node ID="13598714-B10B-477F-865F-29AE0FC9C773" BACKGROUND_COLOR="#FFFFFF" TEXT="1. Preparation" COLOR="#435B66" POSITION="right" STYLE="bubble"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue-Bold" SIZE="21" BOLD="true" /><node ID="4DD462FD-27A7-4ADA-A49C-B6B400E48A82" TEXT="Core Idea" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="0F3420D8-CD0B-4D7E-9D11-9D7DA8DD145B" TEXT="Ideal Outcome &#38; Measurement Scale" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="AF5DB8E0-905B-4EE9-ADE6-784E2ED01BCE" TEXT="Resources" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8D6B969F-3B3E-4857-A952-124125EAC93C" TEXT="Sub-skills (with sequencing)" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="4CD55319-338D-4B06-865B-DDAC7AD2D28C" TEXT="Most critical sub-skills" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="DF13789F-8E88-4021-AE11-2AB32E960DFC" TEXT="Distractions" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="7E41781E-A604-4841-BB21-EED02B349C23" TEXT="Skill Attributes" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="9CD5770F-AEB2-47A8-8BC7-A98AAA4F599F" TEXT="Skill Techniques" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="5B793147-05FA-4D6B-8FB9-E6029A3E7A61" TEXT="Tools required" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="C0A4F262-EA7E-41E8-A5F7-66C02A0848AB" TEXT="Methods of Learning" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="A8BA7C32-A640-4D68-B9B5-701164CD596C" TEXT="Routine" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="BB9CDFD8-BD6F-4423-A1C6-E3645579F65F" BACKGROUND_COLOR="#FFFFFF" TEXT="2. Learning" COLOR="#435B66" POSITION="right" STYLE="bubble"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue-Bold" SIZE="21" BOLD="true" /><node ID="73F55190-8B52-42C1-92A2-C104FB8C065E" TEXT="Best Practices" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8A6A6CCB-1F8F-41D8-99EB-7BFD283ADE9D" TEXT="Mental Models &#38; Hooks" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="D3569F0C-FBDE-4702-9CC6-C203F5C36C17" TEXT="Questions for Experts" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="9CB29FBD-BB51-4AF9-9FBB-CB4583EC6BD5" TEXT="Prescriptive One-pager (Micro)" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="B883D2DE-AF4E-469E-B542-9AF084BA6B6B" TEXT="Practice One-pager (Macro)" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="87B937A9-00E1-4F61-9330-1F85B777329C" BACKGROUND_COLOR="#FFFFFF" TEXT="4. Deliberate Practice" COLOR="#435B66" POSITION="left" STYLE="bubble"><edge COLOR="#DC2128" WIDTH="6" /><font NAME="HelveticaNeue-Bold" SIZE="21" BOLD="true" /></node>
+<node ID="7A5B90D7-5971-4B96-926B-C667E64611CA" BACKGROUND_COLOR="#FFFFFF" TEXT="3. Skill Acquisition" COLOR="#435B66" POSITION="left" STYLE="bubble"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue-Bold" SIZE="21" BOLD="true" /><node ID="BA3C19CF-1B18-4B6C-8F05-EE84EB563AAD" TEXT="All critical tools &#38; their availability" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="FA080FA5-C5B6-4F80-B2CE-B3EBABBBDAAF" TEXT="Feedback loop &#38; Mechanism" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="55B74780-2578-4529-A57D-90CAA059AD30" TEXT="Practice Checklist" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="C42BDBA7-ED2D-40F1-81F2-56D525A21FC2" TEXT="Practise Schedule 
+(Emphasis on Quantity and Speed)" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+</map>
